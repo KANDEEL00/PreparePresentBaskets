0c7c47452035bcb325527ac2b441c6818d21e861
Ahmed Kandeel <1kandeel1@gmail.com>
2023-04-08 01:02:09 +0200

    Add project files.

---
+++ PresentBaskets/PresentBaskets.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Problem
+{
+    // *****************************************
+    // DON'T CHANGE CLASS OR FUNCTION NAME
+    // YOU CAN ADD FUNCTIONS IF YOU NEED TO
+    // *****************************************
+    public static class PresentBaskets
+    {
+        #region YOUR CODE IS HERE
+        /// <summary>
+        /// fill the 2 baskets with the most expensive collection of fruits.
+        /// </summary>
+        /// <param name="W1">weight of 1st basket</param>
+        /// <param name="W2">weight of 2nd basket</param>
+        /// <param name="itemss">Pair of weight (Key) & cost (Value) of each item</param>
+        /// <returns>max total cost to fill two baskets</returns>
+        static public double PreparePresentBaskets(int W1, int W2, KeyValuePair<int, int>[] items)
+        {
+
+        }
+        #endregion
+    }
+}
